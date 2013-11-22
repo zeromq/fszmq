03be1ad963f8ce1fc5026bf679c2be58bf1e3574
Paulmichael Blasucci <pblasucci@gmail.com>
2013-11-21 23:26:25 -0500

    switched unit testing stack to fsunit, fscheck, and nunit

--- build.fsx
+++ build.fsx
@@ -95,99 +95,99 @@ Target "AssemblyInfo" (fun _ ->
 // Clean build results & restore NuGet packages
 
 Target "RestorePackages" (fun _ ->
-    !! "./**/packages.config"
-    |> Seq.iter (RestorePackage (fun p -> { p with ToolPath = "./.nuget/NuGet.exe" }))
+  !! "./**/packages.config"
+  |> Seq.iter (RestorePackage (fun p -> { p with ToolPath = "./.nuget/NuGet.exe" }))
 )
 
-Target "Clean" (fun _ ->
-    CleanDirs ["bin"; "temp"]
-)
+Target "Clean" (fun _ -> CleanDirs ["bin"; "temp"])
 
-Target "CleanDocs" (fun _ ->
-    CleanDirs ["docs/output"]
-)
+Target "CleanDocs" (fun _ -> CleanDirs ["docs/output"])
 
 // --------------------------------------------------------------------------------------
 // Build library & test project
 
 Target "Build" (fun _ ->
-    { BaseDirectories = [__SOURCE_DIRECTORY__]
-      Includes = [ solutionFile +       ".sln"
-                   solutionFile + ".Tests.sln" ]
-      Excludes = [] } 
-    |> MSBuildRelease "" "Rebuild"
-    |> ignore
-    // .fsproj outputs will automatically wind up in the right locations, 
-    // but native libraries need to be moved manually
-    CopyDir "bin/zeromq/x86" "lib/zeromq/x86" (fun _ -> true)
-    CopyDir "bin/zeromq/x64" "lib/zeromq/x64" (fun _ -> true)
+  { BaseDirectories = [__SOURCE_DIRECTORY__]
+    Includes = [ solutionFile +       ".sln"
+                 solutionFile + ".Tests.sln" ]
+    Excludes = [] } 
+  |> MSBuildRelease "" "Rebuild"
+  |> ignore
+  // .fsproj outputs will automatically wind up in the right locations, 
+  // but native libraries need to be moved manually
+  CopyDir "bin/zeromq/x86" "lib/zeromq/x86" (fun _ -> true)
+  CopyDir "bin/zeromq/x64" "lib/zeromq/x64" (fun _ -> true)
 )
 
 // --------------------------------------------------------------------------------------
 // Run the unit tests using test runner
 
 Target "RunTests" (fun _ ->
-    let xunitVersion = GetPackageVersion "packages" "xunit.runners"
-    let xunitPath = sprintf "packages/xunit.runners.%s/tools/xunit.console.clr4.x86.exe" xunitVersion
-    { BaseDirectories = [__SOURCE_DIRECTORY__]
-      Includes = testAssemblies
-      Excludes = [] }
-    |> xUnit (fun p ->
-        { p with
-            ToolPath    = xunitPath
-            ShadowCopy  = true
-            TimeOut     = TimeSpan.FromMinutes 20.
-            OutputDir   = @".\bin\"
-            HtmlOutput  = true })
+  let nunitVersion = GetPackageVersion "packages" "NUnit.Runners"
+  let nunitPath = sprintf "packages/NUnit.Runners.%s/Tools" nunitVersion
+  ActivateFinalTarget "CloseTestRunner"
+
+  { BaseDirectories = [__SOURCE_DIRECTORY__]
+    Includes = testAssemblies
+    Excludes = [] } 
+  |> NUnit (fun p ->
+      { p with
+          ToolName = "nunit-console-x86.exe"
+          ToolPath = nunitPath
+          DisableShadowCopy = true
+          TimeOut = TimeSpan.FromMinutes 20.
+          OutputFile = "TestResults.xml" })
 )
+
+FinalTarget "CloseTestRunner" (fun _ -> ProcessHelper.killProcess "nunit-agent.exe")
 
 // --------------------------------------------------------------------------------------
 // Build a NuGet package
 
 Target "NuGet" (fun _ ->
-    // Format the description to fit on a single line (remove \r\n and double-spaces)
-    let description = description.Replace("\r", "")
-                                 .Replace("\n", "")
-                                 .Replace("  ", " ")
-    let nugetPath = ".nuget/nuget.exe"
-    NuGet (fun p -> 
-        { p with   
-            Authors = authors
-            Project = project
-            Summary = summary
-            Description = description
-            Version = release.NugetVersion
-            ReleaseNotes = String.Join(Environment.NewLine, release.Notes)
-            Tags = tags
-            OutputPath = "bin"
-            ToolPath = nugetPath
-            AccessKey = getBuildParamOrDefault "nugetkey" ""
-            Publish = hasBuildParam "nugetkey"
-            Dependencies = [] })
-        ("nuget/" + project + ".nuspec")
+  // Format the description to fit on a single line (remove \r\n and double-spaces)
+  let description = description.Replace("\r", "")
+                                .Replace("\n", "")
+                                .Replace("  ", " ")
+  let nugetPath = ".nuget/nuget.exe"
+  NuGet (fun p -> 
+      { p with   
+          Authors = authors
+          Project = project
+          Summary = summary
+          Description = description
+          Version = release.NugetVersion
+          ReleaseNotes = String.Join(Environment.NewLine, release.Notes)
+          Tags = tags
+          OutputPath = "bin"
+          ToolPath = nugetPath
+          AccessKey = getBuildParamOrDefault "nugetkey" ""
+          Publish = hasBuildParam "nugetkey"
+          Dependencies = [] })
+      ("nuget/" + project + ".nuspec")
 )
 
 // --------------------------------------------------------------------------------------
 // Generate the documentation
 
 Target "GenerateDocs" (fun _ ->
-    let fsiArgs = if hasBuildParam "Release" then ["--define:RELEASE"] else []
-    executeFSIWithArgs "docs/tools" "generate.fsx" fsiArgs [] |> ignore
+  let fsiArgs = if hasBuildParam "Release" then ["--define:RELEASE"] else []
+  executeFSIWithArgs "docs/tools" "generate.fsx" fsiArgs [] |> ignore
 )
 
 // --------------------------------------------------------------------------------------
 // Release Scripts
 
 Target "ReleaseDocs" (fun _ ->
-    let ghPages      = "gh-pages"
-    let ghPagesLocal = "temp/gh-pages"
-    Repository.clone "temp" (gitHome + "/" + gitName + ".git") ghPages
-    Branches.checkoutBranch ghPagesLocal ghPages
-    CopyRecursive "docs/output" ghPagesLocal true |> printfn "%A"
-    CommandHelper.runSimpleGitCommand ghPagesLocal "add ." |> printfn "%s"
-    let cmd = sprintf """commit -a -m "Update generated documentation for version %s""" release.NugetVersion
-    CommandHelper.runSimpleGitCommand ghPagesLocal cmd |> printfn "%s"
-    Branches.push ghPagesLocal
+  let ghPages      = "gh-pages"
+  let ghPagesLocal = "temp/gh-pages"
+  Repository.clone "temp" (gitHome + "/" + gitName + ".git") ghPages
+  Branches.checkoutBranch ghPagesLocal ghPages
+  CopyRecursive "docs/output" ghPagesLocal true |> printfn "%A"
+  CommandHelper.runSimpleGitCommand ghPagesLocal "add ." |> printfn "%s"
+  let cmd = sprintf """commit -a -m "Update generated documentation for version %s""" release.NugetVersion
+  CommandHelper.runSimpleGitCommand ghPagesLocal cmd |> printfn "%s"
+  Branches.push ghPagesLocal
 )
 
 Target "Release" DoNothing
--- tests/fszmq.tests/UnitTests.fs
+++ tests/fszmq.tests/UnitTests.fs
@@ -19,20 +19,21 @@ Copyright (c) 2011-2013 Paulmichael Blasucci
 namespace fszmq.tests
 
 open ExtCore
+open FsUnit
 open fszmq
+open NUnit.Framework
 open System
-open Xunit
 
 module Miscellany =
     
-  [<Fact>]
+  [<Test>]
   let ``scratch`` () =
-    printfn "This is a test." //TODO: is there a more idiomatically xUnit way to do logging?
-    Assert.True(true)
+    printfn "This is a test." //TODO: is there a more idiomatically NUnit way to do logging?
+    true |> should equal true
 
 module Z85 =
 
-  [<Fact>]
+  [<Test>]
   let ``keypair generation requires sodium`` () =
     
     let HAUSNUMERO = 156384712
@@ -41,25 +42,25 @@ module Z85 =
     let WINXX_ENOTSUP = HAUSNUMERO ||| 1
     
     let err = Assert.Throws<ZMQError>(fun () -> Z85.curveKeyPair() |> ignore)
-    Assert.True(Seq.exists ((=)err.ErrorNumber) [POSIX_ENOTSUP;WINXX_ENOTSUP])
-    Assert.Equal<string>("Not supported",err.Message,StringComparer.InvariantCultureIgnoreCase)
-    
+    [POSIX_ENOTSUP;WINXX_ENOTSUP;] |> should contain err.ErrorNumber 
+    "Not supported" |> should equal err.Message
+
   //TODO: write passing tests, once you figure out libsodium installation
 
-  [<Fact>]
+  [<Test>]
   let ``can encode (binary-to-string)`` () =
     let BINARY  = [| 0x86uy; 0x4Fuy; 0xD2uy; 0x6Fuy; 0xB5uy; 0x59uy; 0xF7uy; 0x5Buy |]
     let encoded = Z85.encode(BINARY)
     printfn "%s" encoded
-    Assert.Equal<string>("HelloWorld",encoded)
+    encoded |> should equal "HelloWorld"
   
   //TODO: add more tests (mostly failing) for Z85.encode
 
-  [<Fact>]
+  [<Test>]
   let ``can decode (string-to-binary)`` () =
     let STRING  = "HelloWorld"
     let decoded = Z85.decode(STRING)
     printfn "%A" decoded
-    Assert.Equal<byte[]>([| 0x86uy; 0x4Fuy; 0xD2uy; 0x6Fuy; 0xB5uy; 0x59uy; 0xF7uy; 0x5Buy |],decoded)
+    decoded |> should equal [| 0x86uy; 0x4Fuy; 0xD2uy; 0x6Fuy; 0xB5uy; 0x59uy; 0xF7uy; 0x5Buy |]
   
   //TODO: add more tests (mostly failing) for Z85.decode
