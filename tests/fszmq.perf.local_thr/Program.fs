bc3f35d539dae4a0a34fee0262453beac01b336c
Paulmichael Blasucci <pblasucci@gmail.com>
2013-12-18 21:01:05 -0600

    added perf tests

--- tests/fszmq.perf.local_thr/Program.fs
+++ tests/fszmq.perf.local_thr/Program.fs
@@ -16,24 +16,13 @@ along with fszmq. If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (c) 2011-2013 Paulmichael Blasucci
 ------------------------------------------------------------------------ *)
-module fszmq.perf.inproc_thr.Program
+module fszmq.perf.local_thr.Program
 
 open fszmq
 open fszmq.Socket
 open fszmq.Timing
-open System.Diagnostics
-open System.Threading
 
 (* _ zeromq ____________________________________________________________ *)
-
-let [<Literal>] ENDPOINT = "inproc://thr_test"
-
-let worker (state:obj) =
-  let messageSize,messageCount,context = downcast state
-  use socket = Context.push context
-  connect socket ENDPOINT
-  let frame = Array.zeroCreate messageSize
-  for _ in 1L .. messageCount do frame |>> socket 
 
 let recvMsg messageSize socket =
     let msg = Option.get <| tryRecv socket messageSize ZMQ.WAIT
@@ -41,25 +30,19 @@ let recvMsg messageSize socket =
     //      But when using the ZMQ.WAIT flag, its a pretty safe bet.
     if Array.length msg <> messageSize then failwith "message of incorrect size received"
 
-let runTest messageSize messageCount =
+let runTest address messageSize messageCount =
   use context = new Context()
   use socket  = Context.pull context
-  bind socket ENDPOINT
-  
-  printfn "message size: %i [B]"  messageSize
-  printfn "message count: %i"     messageCount
+  Socket.bind socket address
 
-  let thread = Thread(ParameterizedThreadStart(worker))
-  thread.Start((messageSize,messageCount,context))
-  
   recvMsg messageSize socket
   let elapsed = execTimed (fun () -> for _ in 1L .. (messageCount - 1L) do recvMsg messageSize socket)
 
-  thread.Join()
-
   let throughput  = uint32 ((float messageCount) / (float elapsed) * 1000000.0)
   let megabits    = (float (throughput * (uint32 messageSize) * 8u)) / 1000000.0
-
+  
+  printfn "message size: %i [B]" messageSize
+  printfn "message count: %i" messageCount
   printfn "mean throughput: %d [msg/s]"  throughput
   printfn "mean throughput: %.3f [Mb/s]" megabits 
 
@@ -68,13 +51,13 @@ let runTest messageSize messageCount =
 let [<Literal>] FAIL = -1
 let [<Literal>] OKAY =  0
 
-let (|SizeCount|_|) = function
-  | [| size; count; |] -> Some(int size,int64 count)
-  | _                  -> None
+let (|Args|_|) = function
+  | [| address; size; count; |] -> Some(address,int size,int64 count)
+  | _                           -> None
 
 [<EntryPoint>]
 let main = function
-  | SizeCount(size,count) ->  runTest size count
-                              OKAY
-  | _ -> printfn "usage: inproc_thr <message-size> <message-count>"
+  | Args(address,size,count) -> runTest address size count
+                                OKAY
+  | _ -> printfn "usage: local_lat <bind-address> <message-size> <message-count>"
          FAIL
