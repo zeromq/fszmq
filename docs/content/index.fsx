20841d934241bdfdffc181fcc904787c8b555564
Paulmichael Blasucci <pblasucci@gmail.com>
2015-06-13 14:51:16 -0400

    tweaked windows sln; added copyright footer to main docs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -70,10 +70,11 @@ the [F#-specific zguide code samples][zguidefs] have been [collected here as a c
 Contributing and copyright
 --------------------------
 
-The project is hosted on [GitHub][gh] where you can [report issues][issues], fork
-the project and submit pull requests. If you're adding new public API, please also
-consider adding [samples][content] that can be turned into a documentation. You might
-also want to read [library design notes][readme] to understand how it works.
+The project is hosted on [GitHub][gh] where you can [report issues][issues] 
+(or, better yet, fork the project and submit pull requests. If you're adding a 
+new public API, please also consider adding [samples][content] that can be 
+turned into help docs. Also be sure to read the [contributor notes][contrib] 
+before getting started.)
 
 The fszmq library is available under the Mozilla Public License, v. 2.0.
 For more information see the [license file][license] in the GitHub repository.
@@ -82,10 +83,15 @@ The documentation accompanying fszmq, and any sample code contained therein, is 
 the MIT LICENSE which allows modification and reuse for both commercial non-commercial purposes.
 For more information see the [documentation license file][docslicense] in the GitHub repository.
 
+---------------------------------------------------------------------------
+
+###### Copyright © 2011-2015 Paulmichael Blasucci.
+
   [content]: https://github.com/zeromq/fszmq/tree/master/docs/content
   [gh]: https://github.com/zeromq/fszmq
   [issues]: https://github.com/zeromq/fszmq/issues
   [readme]: https://github.com/zeromq/fszmq/blob/master/README.md
+  [contrib]: https://github.com/zeromq/fszmq/blob/master/CONTRIBUTING.md
   [license]: https://github.com/zeromq/fszmq/blob/master/LICENSE.txt
   [docslicense]: https://github.com/zeromq/fszmq/blob/master/docs/files/LICENSE.txt
   [zguide]: http://zguide.zeromq.org/page:all
